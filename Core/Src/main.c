561653ff4a01363ab371b3ae3cc18d4882d0f3a5
wejden-kerkeni <158564784+wejden-kerkeni@users.noreply.github.com>
2024-02-06 22:56:03 +0100

    Update main.c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -94,26 +94,6 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 	}
 
 
-	if (GPIO_Pin == GPIO_PIN_1)
-	{
-
-		AudioState = AUDIO_STATE_VOLUME_UP;
-	}
-	if (GPIO_Pin == GPIO_PIN_2)
-		{
-
-			AudioState = AUDIO_STATE_VOLUME_DOWN;
-		}
-	if (GPIO_Pin == GPIO_PIN_3)
-		{
-
-			AudioState = AUDIO_STATE_NEXT;
-		}
-	if (GPIO_Pin == GPIO_PIN_5)
-		{
-
-			AudioState = AUDIO_STATE_PREVIOUS;
-		}
 }
 /* USER CODE END 0 */
 
@@ -335,8 +315,7 @@ static void MX_GPIO_Init(void)
 
   /*Configure GPIO pins : PA0 PA1 PA2 PA3
                            PA5 */
-  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3
-                          |GPIO_PIN_5;
+  GPIO_InitStruct.Pin = GPIO_PIN_0;
   GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
   GPIO_InitStruct.Pull = GPIO_PULLDOWN;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
@@ -344,18 +323,6 @@ static void MX_GPIO_Init(void)
   /* EXTI interrupt init*/
   HAL_NVIC_SetPriority(EXTI0_IRQn, 0, 0);
   HAL_NVIC_EnableIRQ(EXTI0_IRQn);
-
-  HAL_NVIC_SetPriority(EXTI1_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(EXTI1_IRQn);
-
-  HAL_NVIC_SetPriority(EXTI2_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(EXTI2_IRQn);
-
-  HAL_NVIC_SetPriority(EXTI3_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(EXTI3_IRQn);
-
-  HAL_NVIC_SetPriority(EXTI9_5_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
 
 }
 
